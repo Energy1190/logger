e700e7ff6af2fc69e0eb086ff8f928f8284d68f4
Energy1190 <energyneo0@gmail.com>
2018-10-18 16:28:16 +0300

    +

--- create_index/run.py
+++ create_index/run.py
@@ -52,7 +52,8 @@ def install_lib(name:str,recursion=0,pip='pip3'):
         time_start=time_start)
     return lib
 
-logging.basicConfig(level=logging.INFO)
+logging = logging.getLogger(__name__)
+logging.setLevel(logging.INFO)
 
 msg(__name__,'init','Start.', logging.info, time_start=time_start)
 msg(__name__,'init:import','Start.', logging.info, time_start=time_start)
